a974081d9fa8f10fe552e40770d5c83612c59cad
ZohaibAhmad <ahmadzohaib369@gmail.com>
2022-02-24 17:12:50 +0500

    uploading common folder

---
+++ components/input-presentation/FloatingLabelInput.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FloatingLabelInput = () => {
+  return (
+    <View>
+      <Text>FloatingLabelInput</Text>
+    </View>
+  )
+}
+
+export default FloatingLabelInput
+
+const styles = StyleSheet.create({})
